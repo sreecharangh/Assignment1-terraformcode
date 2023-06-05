c1c8ecba84a1f29fea04bf4a8fccaedfe09e13e7
sreecharangh <126445147+sreecharangh@users.noreply.github.com>
2023-06-05 01:18:16 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "app" {
   ami           = "ami-0806bc468ce3a22ec"
   instance_type = "t2.micro"
   subnet_id     = "subnet-02107dcc77c5746f5"
-  security_group_ids = ["sg-08ba0abbbd57ef070"]
+  vpc_security_group_ids = ["sg-08ba0abbbd57ef070"]
   key_name      = "key1"
 
   tags = {
